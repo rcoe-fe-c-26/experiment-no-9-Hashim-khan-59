6a136ad632c0a9d8d608c5cf4d77472a1abb0b3a
Hashim-khan-59 <hashim@eng.rizvi.edu.in>
2026-02-09 12:32:24 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 # AIM: Design a Python program to compute 
 # the factorial of a given integer N.
-# Coder:
-# Date:
+# Coder:Hashim Tahir Khan 
+# Date:30-01-26
 
 print("--- Factorial Finder ---\n")
+num = int(input("Enter Number: "))
 
+if num < 0:
+    print(f"Factorial of {num} is Not Defined")
+else:
+    factorial = 1
+    for i in range(1, num + 1):
+        factorial *= i
+    print(f"Factorial of {num} is {factorial}")
+  
 
 # Write your code here
+
